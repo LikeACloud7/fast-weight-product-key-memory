ec878e6a22eb030594cb6fec953b1970e5a7ca80
Tianyu Zhao <zhaoty.ting@gmail.com>
2026-02-24 03:10:01 +0000

    Fix dependencies

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ pip install vllm==0.8.3 tensorboard
 pip install wheel packaging
 pip install flash-attn==2.7.4.post1 --no-build-isolation
 pip install flashinfer-python -i https://flashinfer.ai/whl/cu124/torch2.6/
+pip install deepspeed==0.17.6
+pip install transformers==4.57.1
+pip install flash-linear-attention==0.3.2
 
 pip install -r requirements.txt
